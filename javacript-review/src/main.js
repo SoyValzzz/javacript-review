2ef2e64d88ae729e13d9bad7e297231e9bc5141b
SoyValzzz <u20211a962@upc.edu.pe>
2024-09-11 19:55:04 -0500

    feat(main): updated behavior including customer creating and last order value assignment.

--- javacript-review/src/main.js
+++ javacript-review/src/main.js
@@ -1,7 +1,12 @@
 import {SalesOrder} from "./sales/domain/model/sales-order.js";
+import {Customer} from "./crm/domain/model/customer.js";
 
+const customer = new Customer({name: "John Doe"});
 const order = new SalesOrder({customerId: 1});
 order.addItem({productId: 1, quantity: 2, unitPrice: 100});
 order.addItem({productId: 2, quantity: 1, unitPrice: 50 });
+customer.lastOrderTotalPrice = order.calculateTotalPrice();
 
 console.log(`Sale Order Total Price is ${order.calculateTotalPrice()}`);
+console.log(`The Customer ${customer.name} identified with ID ${customer.id} has a total price os ${customer.lastOrderTotalPrice} on its last order`)
+
